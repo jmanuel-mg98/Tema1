c5756ad50753f9323a8bf228679aa725d5d8cbb2
josem.martin <josem.martin@106-21.info2>
2023-10-20 14:24:38 +0200

    ej57

---
+++ src/ejercisentregat1/Ejercise5.java
@@ -0,0 +1,13 @@
+package ejercisentregat1;
+
+public class Ejercise5 {
+
+	public static void main(String[] args) {
+		System.out.println("i´m Jose Manuel");
+		System.out.println("i live in seville");
+		System.out.println("");
+		System.out.println("my telephone number is 638893260");
+
+	}
+
+}
---
+++ src/ejercisentregat1/Ejercise7.java
@@ -0,0 +1,14 @@
+package ejercisentregat1;
+
+public class Ejercise7 {
+
+	public static void main(String[] args) {
+		System.out.println("******* *     *");
+		System.out.println("   *    **   **");
+		System.out.println("   *    * * * *");
+		System.out.println("   *    *  *  *");
+		System.out.println("*  *    *     *");
+		System.out.println("****    *     *");
+	}
+
+}
