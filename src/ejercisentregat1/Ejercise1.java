57d24b3b200ccb163294777223503a348a613272
josem.martin <josem.martin@106-21.info2>
2023-10-20 14:00:30 +0200

    creacion repositorio

---
+++ src/ejercisentregat1/Ejercise1.java
@@ -0,0 +1,11 @@
+package ejercisentregat1;
+
+public class Ejercise1 {
+
+	public static void main(String[] args) {
+		System.out.print("hello, how are you?");
+		System.out.println("fine thanks");
+
+	}
+
+}
---
+++ src/ejercisentregat1/Ejercise2.java
@@ -0,0 +1,11 @@
+package ejercisentregat1;
+
+public class Ejercise2 {
+
+	public static void main(String[] args) {
+		System.out.println("hello, how are you?");
+		System.out.println("fine thanks.");
+
+	}
+
+}
