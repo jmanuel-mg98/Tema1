4b235a7dbc4ec63dc9d1224e01e612e168de0e17
josem.martin <josem.martin@106-21.info2>
2023-10-20 14:41:32 +0200

    act

--- src/ejerciset1p2/Ejercise115.java
+++ src/ejerciset1p2/Ejercise115.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class Ejercise115 {
 
 	public static void main(String[] args) {
-		cs Scanner = new Scanner(System.in);
+		Scanner cs = new Scanner(System.in);
 		System.out.print("nota primer trimestre:");
-		int nota1 = ne
+		int nota1 = cs.nextInt();
+		
 
 	}
 
