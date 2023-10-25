4bfb287b116daaee9a50b8b77caabb68f5d168ec
josem.martin <josem.martin@106-21.info2>
2023-10-25 08:49:24 +0200

    sdfg

--- src/ejerciset1p2/Ejercise115.java
+++ src/ejerciset1p2/Ejercise115.java
@@ -6,8 +6,18 @@ public class Ejercise115 {
 
 	public static void main(String[] args) {
 		Scanner cs = new Scanner(System.in);
-		System.out.print("nota primer trimestre:");
+		System.out.print("nota primer trimestre: ");
 		int nota1 = cs.nextInt();
+		System.out.print("nota segundo trimestre: ");
+		int nota2 = cs.nextInt();
+		System.out.print("nota tercer trimestre: ");
+		int nota3 = cs.nextInt();
+		int notame = (nota1 + nota2 + nota3)/3;
+		double  notameb = (double)(nota1 + nota2 + nota3)/3;
+		System.out.println("nota media aparecida en boletin:" + notame );
+		System.out.print("nota media aparecida en expediente academico:" + notameb );
+		
+		
 		
 
 	}
